44badd527000e3edb7ee6f17ab18d9e8deb18b0d
YingChangCui <78188786+YingChangCui@users.noreply.github.com>
2021-05-06 23:06:26 -0700

    Customize order and added all the buttons

---
+++ CashierApp/src/main/java/com/example/CashierApp/CashierAppController.java
@@ -0,0 +1,58 @@
+package com.example.CashierApp;
+
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.util.Optional;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64.Encoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BeanPropertyBindingResult;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.Getter;
+import lombok.Setter;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class CashierAppController {
+
+    @GetMapping
+    public String getAction(Model model) {
+        log.info("Application Started");
+        return "order" ;
+
+    }
+
+    @PostMapping
+    public String postAction(@RequestParam(value="action", required=true) String action,
+                            Errors errors, Model model, HttpServletRequest request) {
+    
+        log.info("test");
+                                
+    
+        return "order";
+
+    }
+}
---
+++ CashierApp/src/main/java/com/example/CashierApp/Order.java
@@ -0,0 +1,9 @@
+package com.example.CashierApp;
+
+public class Order {
+    private String drink;
+    private String size;
+    private String milk;
+    private String message;
+    private String price;
+}
