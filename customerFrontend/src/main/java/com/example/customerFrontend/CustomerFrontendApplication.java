844273fce6672f974e0db4d113160b2358c3c7cc
xuefengxu-21 <xuefeng.xu@sjsu.edu>
2021-05-06 17:38:21 -0700

    customer front end

---
+++ customerFrontend/src/main/java/com/example/customerFrontend/CustomerFrontendApplication.java
@@ -0,0 +1,14 @@
+package com.example.customerFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+@Component
+@SpringBootApplication
+public class CustomerFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFrontendApplication.class, args);
+	}
+
+}
